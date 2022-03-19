e9ef5847200d6be08628b35aea8351e09c18fdd2
Ezy93 <giordani001@gmail.com>
2022-03-19 13:03:57 +0100

    eseguiti tutti i milestone principali

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ Stampare all'interno di una lista, un item per ogni todo. Se la proprietà done 
 !check  Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
 !Bonus:
-1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+!check 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa) */
 
 
@@ -46,8 +46,8 @@ const app = new Vue({
             this.toDoList.splice(index,1);
         },
 
-        toDoPerformed(index){
-            
+        toDoPerformed(){
+            /* cercare soluzione per accedere alla proprieta done dell'oggetto e cambirne il valore */
         },
 
     },
