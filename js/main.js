a5d5d04463eac0a7aab6449ff36f3e4f9dc4ead9
Ezy93 <giordani001@gmail.com>
2022-03-19 12:31:29 +0100

    aggiunta possibilita di inserimento todo col tasto enter

--- js/main.js
+++ js/main.js
@@ -35,6 +35,7 @@ const app = new Vue({
                     text: toDoContent,
                     done: false
                 },)
+                this.newToDo = "";
                 
             }
             
